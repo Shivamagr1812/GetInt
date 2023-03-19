c1c2d23ea5b07beefae7789c4717ba5f1e7a652c
Shivam <eshivam360@gmail.com>
2023-03-19 17:23:08 +0530

    welcome added

--- client/src/Home.js
+++ client/src/Home.js
@@ -17,6 +17,7 @@ const Home = ()=>{
 
     return (
         <div className="homePage">
+            <h1 className="welcome">Welcome User!</h1>
             <div className="dialogBox">
                 <header><h1>Choose your role</h1></header>
                 <div className="option">
