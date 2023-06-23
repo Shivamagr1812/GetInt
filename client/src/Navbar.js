37d6426cd6efe99fafb7a20f12bcaa34418083fa
Amrisha <amrishavardiya09@gmail.com>
2023-06-23 18:36:59 +0530

    responsive navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,17 +1,31 @@
 import { Link } from "react-router-dom";
-
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = ()=>{
+
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+
     console.log("Working Fine");
     return (
         <nav>
             <div className="title"><Link to="/about" ><img src={require('./assets/logos/png/logo-white-no-bg.png')} alt="GetIntern" /></Link></div>
-            <ul className="navbar">
+            <nav className="navbar" ref={navRef}>
                 <Link to="/" className="home">Home</Link>
                 <Link to="/interns">Available Internships</Link>
                 <Link to="/new-intern">Add Internship</Link>
                 {/* <Link to="/register">Login/Register</Link> */}
                 <Link to="/login">Login/Register</Link>
-            </ul>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes/>
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <FaBars/>
+            </button>
         </nav>
     );
 }
