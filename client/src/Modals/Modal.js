31403dc85d546f317cee661ac311d79bb805ebbb
Amrisha <amrishavardiya09@gmail.com>
2023-06-25 17:24:04 +0530

    apply form css

--- client/src/Modals/Modal.js
+++ client/src/Modals/Modal.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState } from "react";
 import axios from "axios";
+import './Modals.css';
+import { FaBars, FaTimes } from "react-icons/fa";
+
 
 function Modal(props) {
     const closeModal = () => {
@@ -35,37 +38,60 @@ function Modal(props) {
     <div className='popup'>
         <div onClick={closeModal} className="overlay"></div>
         <div className='popup-inner'>
-            <button onClick={closeModal} className='close-btn'>close me</button>
+            <button onClick={closeModal} className='close-btn'><FaTimes/></button>
             <h3>Submit your Resume</h3>
-                    <form className="application" onSubmit={submitForm}>
-                        <label htmlFor="name">Name:</label>
+            <form className="application" onSubmit={submitForm}>
+                <div className="formRow">
+                    <div className="inputData">
                         <input type="text" name="name" id="name" value={name} required onChange={
                             (e)=>{
                                 setName(e.target.value);
                             }
                         } />
-                        <label htmlFor="email">Email:</label>
+                        <div class="underline"></div>
+                        <label htmlFor="name">Name</label>
+                    </div>
+                    <div className="inputData">
                         <input type="email" name="email" id="email" value={email} required onChange={
                             (e)=>{ 
                                 setEmail(e.target.value);
                             }
                         } />
-                        <label htmlFor="branch">Branch:</label>
+                        <div class="underline"></div>
+                        <label htmlFor="email">Email</label>
+                    </div>
+                </div>
+                <div className="formRow">
+                    <div className="inputData">
                         <input type="text" name="branch" id="branch" value={branch} required onChange={
                             (e)=>{
                                 setBranch(e.target.value);
                             }
                         } />
-                        <label htmlFor="role">Role:</label>
+                        <div class="underline"></div>                    
+                        <label htmlFor="branch">Branch</label>        
+                    </div>
+                    <div className="inputData">
                         <input type="text" name="role" id="role" value={role} required onChange={
                             (e)=>{
                                 setRole(e.target.value);
                             }
                         } />
-                        <label htmlFor="resume">Resume:</label>
-                        <input type="file" name="resume" id="resume" ref={fileInput} required />
-                        <button type='submit'>Submit</button>
-                    </form>
+                        <div class="underline"></div>
+                        <label htmlFor="role">Role</label>
+                    </div>
+                </div>
+                <div className="formRow rowResume">
+                    <label htmlFor="resume">Resume: </label>
+                    <input className="resume" type="file" name="resume" id="resume" ref={fileInput} required />
+                </div>
+                <div className="formRow submit-btn">
+                    <div className="inputData">
+                        <div class="inner"></div>
+                        <button type='submit'>Submit</button>        
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
   ) : "";
